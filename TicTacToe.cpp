a31d4e34e652ac225e0cb8da572034513181767c
ShairaT <31709410+ShairaT@users.noreply.github.com>
2017-09-06 22:07:47 -0300

    Add files via upload
    Add: selection of character and colors

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,6 +6,9 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <stdlib.h>
+#include <windows.h>
+
 using namespace std;
 
 char board[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
@@ -13,21 +16,34 @@ char board[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 int check_win();
 void board_status();
 void gameplay(char, int, int);
+void color(int X);
+void Select_character();
+char showBoard(char bord);
+char player1;
+char player2;
+
 
 /*
  * Driver Function.
  */
 int main()
 {
+    bool play_again = true;
+    int yes_or_no = 0;
+
+    while (play_again == true){
+    system("cls");
 	int player = 1;
 	int i, box_no;
 
 	char mark_x_o;
+    Select_character();
     //gameplay();
-    cout << endl << " Get Ready to Play Tic Tac Toe " << endl << endl;
-
 	do
 	{
+	    system("cls");
+	    color(14);
+        cout << endl << " Get Ready to Play Tic Tac Toe " << endl << endl;
 		board_status();
 		player = player % 2 ? 1 : 2;
         gameplay(mark_x_o,box_no,player);
@@ -36,28 +52,41 @@ int main()
 
 	}while (i == -1);
 
+    system("CLS");
+    color(14);
+    cout << endl << " Get Ready to Play Tic Tac Toe " << endl << endl;
 	board_status();
-
+    color(15);
 	if (i == 1)
-		cout << "~~~Player " << --player << " Won" << endl;
+		cout << endl << " Player " << --player << " Won" << endl;
 	else
-		cout << "~~~Game Draw!" << endl;
+		cout << endl <<  " Game Draw!" << endl;
+    cout << " Another game? Press 1 for yes or press 2 for no" << endl;
+    cin >> yes_or_no;
+    while (yes_or_no != 1 && yes_or_no != 2){
+        cout << "Invalid number! please press again" << endl;
+        cin >> yes_or_no;
+    }
+    if (yes_or_no == 2)
+        play_again = false;
+	}
+	cout << "Thanks for play!" << endl;
 	return 0;
 }
 
 void gameplay(char mark_x_o, int box_no,int player){
-    
-    cout << "Player" << player << ", enter a number to mark_x_o: ";
+
+    cout << endl << " Player " << player << ", enter a number to place an " << player1 << " or " << player2 << ": ";
     cin >> box_no;
     while (cin.fail() || box_no < 1 || box_no > 9 || box_no!= (int)board[box_no]-'0'){
         cin.clear();
         cin.ignore();
-        cout << "Invalid Move!" << endl;
-        cout << "Player" << player << ", enter a number to mark_x_o: ";
+        cout << " Invalid Move!" << endl;
+        cout << " Player " << player << ", enter a number to " << player1 << " or " << player2 << ": ";
         cin >> box_no;
     }
-    
-    mark_x_o = player == 1 ? 'X' : 'O';
+
+    mark_x_o = player == 1 ? player1 : player2;
 
     if (box_no == 1 && board[1] == '1')
         board[1] = mark_x_o;
@@ -125,9 +154,9 @@ int check_win ()
                 done = false;
                 break;
             }
-            
+
         }
-                 
+
 		if (done) return 0;
     }
 
@@ -139,23 +168,141 @@ int check_win ()
  */
 void board_status ()
 {
-	
-    cout << endl;
-	cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
+    char vertical = ((char)186);
+    char horizontal = ((char)205);
+    char mid = ((char)206);
 
-	cout << "       |       |       " << endl;
+    color(15);
 
-	cout << "      " << board[1] << "|      " << board[2] << "|     " << board[3] << endl;
+	cout << "       " << vertical << "       " << vertical << "       " << endl;
 
-	cout << "_______|_______|________" << endl;
-	cout << "       |       |        " << endl;
+	cout << "      " << showBoard(board[1]);
 
-	cout << "      " << board[4] << "|      " << board[5] << "|     " << board[6] << endl;
+	color(15);
 
-	cout << "_______|_______|________" << endl;
-	cout << "       |       |        " << endl;
+	cout << vertical;
 
-	cout << "      " << board[7] << "|      " << board[8] << "|     " << board[9] << endl;
+	cout << "      " << showBoard(board[2]);
 
-	cout << "       |       |        " << endl << endl;
+	color(15);
+
+	cout << vertical;
+
+	cout << "     " << showBoard(board[3]);
+
+	color(15);
+
+	cout << endl;
+
+	for(int i = 0; i < 7; i ++) cout << horizontal;
+
+	cout << mid;
+
+	for(int i = 0; i < 7; i ++) cout << horizontal;
+
+	cout << mid;
+
+	for(int i = 0; i < 7; i ++) cout << horizontal;
+
+	cout << endl;
+
+	cout << "       " << vertical << "       " << vertical << "        " << endl;
+
+    cout << "      " << showBoard(board[4]);
+
+	color(15);
+
+	cout << vertical;
+
+	cout << "      " << showBoard(board[5]);
+
+	color(15);
+
+	cout << vertical;
+
+	cout << "     " << showBoard(board[6]);
+
+	color(15);
+
+	cout << endl;
+
+	for(int i = 0; i < 7; i ++) cout << horizontal;
+
+	cout << mid;
+
+	for(int i = 0; i < 7; i ++) cout << horizontal;
+
+	cout << mid;
+
+	for(int i = 0; i < 7; i ++) cout << horizontal;
+
+	cout << endl;
+
+    cout << "      " << showBoard(board[7]);
+
+	color(15);
+
+	cout << vertical;
+
+	cout << "      " << showBoard(board[8]);
+
+	color(15);
+
+	cout << vertical;
+
+	cout << "     " << showBoard(board[9]);
+
+	color(15);
+
+	cout << endl;
+
+	cout << "       " << vertical << "       " << vertical << "       " << endl;
+
+
+
+}
+
+void color(int X)
+{
+    SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),X);
+}
+
+char showBoard(char bord)
+{
+
+        if(bord == player1)
+        {
+            color(11);
+        }
+        else if(bord == player2)
+        {
+            color(12);
+        }
+        else
+        {
+            color(15);
+        }
+        return bord;
+}
+void Select_character(){
+
+    color(15);
+    cout << " Player 1 please select your character!: ";
+    cin >> player1;
+    while ((int)player1 >47 && (int)player1 <58){
+    cout << " You can't select a number, please select another!: ";
+    cin >> player1;
+    }
+    cout << " Player 2 please select yours too!: ";
+    cin >> player2;
+    while (player1 == player2){
+        cout << " You can't have the same character as player 1, please select another!: ";
+        cin >> player2;
+        while ((int)player2 >47 && (int)player2 <58){
+            cout << " You can't select a number, please select another!: ";
+            cin >> player2;
+    }
+    }
+
+    return;
 }
